60ff8b3212f8d2daeff9d6b48d4140c960b28cc5
ffura2121 <153360106+ffura2121@users.noreply.github.com>
2025-10-29 23:26:36 +0200

    Add files via upload

---
+++ nix-study-py/task2/task2.py
@@ -0,0 +1,6 @@
+firstname = input("Ввведіть ім'я: ")
+lastname = input("Ввведіть ім'я: ")
+
+print(f"Ваше ім'я це {firstname} та ваше прізвище {lastname}")
+
+print("Ваше ім'я це {} та ваше прізвище {}".format(firstname,lastname))
---
+++ nix-study-py/task3/task3.py
@@ -0,0 +1,7 @@
+num1 = 22.32131
+num2 = 58.60125
+num3 = 34.0
+
+print(round(num1,2))
+print(round(num2,1))
+print(round(num3))
